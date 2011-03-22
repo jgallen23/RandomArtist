c4dd7d2ce349e9aa8e31d0f3eec12fcd763d0dc9
Greg Allen <mail@jga23.com>
2011-03-21 20:22:15 -0700

    initial commit

---
+++ Classes/RandomArtistAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  RandomArtistAppDelegate.h
+//  RandomArtist
+//
+//  Created by Gregamel on 3/21/11.
+//  Copyright Boeing 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface RandomArtistAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
